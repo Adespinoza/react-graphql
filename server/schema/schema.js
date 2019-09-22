0c0869d3ec9667d55b5d89442c4e187e85af84e3
Adam Espinoza <adamespi@usc.edu>
2019-09-21 18:49:13 -0700

    🚀 updated schema + dummy data

---
+++ server/schema/schema.js
@@ -0,0 +1,37 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'Two States', genre: 'Drama', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+      }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code from Database -- right now it's dummy data
+        return books.find(item => item.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
