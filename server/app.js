2968c835a47248cddbd0b84ed7a40fb52ba8adc4
Adam Espinoza <adamespi@usc.edu>
2019-11-18 15:56:19 -0800

    🚨 squashed linter warnings

--- server/app.js
+++ server/app.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect(process.env.MONGOHQ_URL);
-mongoose.connection.once('open', () => {
-    console.log('Conneted to database');
-  });
+require("dotenv").config();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true,
-}));
+mongoose.connect(process.env.MONGOHQ_URL, {useNewUrlParser: true});
+mongoose.connection.once("open", () => {
+  console.log("Conneted to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
   console.log(`Listening at port ${process.env.PORT}`);
